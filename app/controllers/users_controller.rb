ca5334f1c5e86201ef166c7b820c7136dee597c3
Henrique Balmant <henrique.balmant@gmail.com>
2019-04-03 13:42:18 -0300

    Fix sign up and null: false for shouts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,13 @@
 class UsersController < Clearance::UsersController
+    def new
+        @user = User.new
+    end
+
     def show
         @user = User.find(params[:id])
         @shouts = @user.shouts
     end
-    
+        
     private
 
     def user_params
