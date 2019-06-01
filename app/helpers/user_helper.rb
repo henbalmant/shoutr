ea2c4e08be2afb49987f97b16f32e95b7aa61f75
Henrique Balmant <henrique.balmant@gmail.com>
2019-06-01 18:00:30 -0300

    Add beauty follow button

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,9 +1,9 @@
 module UserHelper
     def follow_button(user)
         if current_user.following?(user)
-            button_to "Unfollow", unfollow_user_path, method: :delete
+            button_to "Unfollow", unfollow_user_path, method: :delete, class: "btn-follow"
         else
-            button_to "Follow", follow_user_path
+            button_to "Follow", follow_user_path, class: "btn-follow"
         end
     end
 end
